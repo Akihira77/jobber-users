bafdd80117c0b1e4ca5c291a0ccb8f89a49e7f92
Akihira77 <tik.dikawahyu123@gmail.com>
2024-05-29 20:05:57 +0700

    refactore code add close connection db before/after program exit

--- src/app.ts
+++ src/app.ts
@@ -19,14 +19,23 @@ const main = async (): Promise<void> => {
             moduleName ?? "Users Service",
             "debug"
         );
-    cloudinary.v2.config({
-        cloud_name: CLOUD_NAME,
-        api_key: CLOUD_API_KEY,
-        api_secret: CLOUD_API_SECRET
-    });
-    await databaseConnection(logger);
-    const app: Express = express();
-    await start(app, logger);
+    try {
+        cloudinary.v2.config({
+            cloud_name: CLOUD_NAME,
+            api_key: CLOUD_API_KEY,
+            api_secret: CLOUD_API_SECRET
+        });
+        const db = await databaseConnection(logger);
+        const app: Express = express();
+        await start(app, logger);
+
+        process.once("exit", async () => {
+            await db.connection.close();
+        });
+    } catch (error) {
+        logger("app.ts - main()").error("Initiate app error", error);
+        process.exit(1);
+    }
 };
 
 main();
--- src/database.ts
+++ src/database.ts
@@ -1,23 +1,18 @@
 import { DATABASE_URL } from "@users/config";
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 import { Logger } from "winston";
 
 export const databaseConnection = async (
     logger: (moduleName: string) => Logger
-): Promise<void> => {
+): Promise<Mongoose> => {
     try {
-        await mongoose.connect(`${DATABASE_URL}`);
-        logger("database.ts - databaseConnection()").info(
-            "UsersService MongoDB is connected."
-        );
-
-        process.once("exit", async () => {
-            await mongoose.connection.close();
-        });
+        const db = await mongoose.connect(`${DATABASE_URL}`);
+        return db;
     } catch (error) {
         logger("database.ts - databaseConnection()").error(
             "UsersService databaseConnection() method error:",
             error
         );
+        throw error;
     }
 };
